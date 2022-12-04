b85c7f00030ebb2a7f2fe183ee066536cfee5c9c
Edward <73746306+WangEdward@users.noreply.github.com>
2022-12-04 22:07:55 +0800

    Add http exceptions

---
+++ src/main/java/com/example/survey/exception/util/HttpResponse.java
@@ -0,0 +1,25 @@
+package com.example.survey.exception.util;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class HttpResponse implements Serializable {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss", timezone ="Asia/Taipei")
+    private Date timestamp;
+    private int httpStatusCode;
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+
+
+}
