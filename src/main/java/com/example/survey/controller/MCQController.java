a997ddd89a06a4c1abc93ceb33c74415588e4188
Edward <73746306+WangEdward@users.noreply.github.com>
2023-02-19 18:00:41 +0800

    fix: allow cross origin

--- src/main/java/com/example/survey/controller/MCQController.java
+++ src/main/java/com/example/survey/controller/MCQController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/mcq")
 @AllArgsConstructor
